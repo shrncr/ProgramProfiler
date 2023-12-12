eaa10dcaec7d3219a0af3d9d89f273e71be5d05d
shrncr <110135122+shrncr@users.noreply.github.com>
2023-12-12 08:45:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import os
 from ProgramProfiler import *
 
 np.set_printoptions(threshold=np.inf)
-testf = open(r'student2.txt').read().split("\n") #testfile
+testf = open(r'student.txt').read().split("\n") #testfile
 
 programMatrices = createProgramMatrices()
-print(1)
 progProf = toProgramProfile(programMatrices)
-print(2)
 
 testMat = curToMatrix(testf,138)
-print(3)
 FrobNorm(progProf,testMat)
 
 
