224bd5ca1c4159070f1db8c8c1c6ccfaa5d7c0fb
shrncr <loveyawink@gmail.com>
2023-12-13 09:27:34 -0600

    commenting source code

--- ProgramProfiler.py
+++ ProgramProfiler.py
@@ -3,7 +3,7 @@ import os
 from numpy import linalg as LA
 from scipy.interpolate import CubicSpline
 
-keys = { #key words
+keys = { #key words point to key concepts. if/else/elif all have values of 4 as they are all conditionals.
     "def __init": 0,
     "def": 1,
     "import": 2,
@@ -20,18 +20,16 @@ keys = { #key words
     "=":10
 }
 
-def FrobNorm(programProfile, newProg):
-    #maybe add a shape check to make sure rows = 14
-    #wait i might not need that
-    profileNorm = LA.norm(programProfile)
+def FrobNorm(programProfile, newProg): #from norm, aka euclidean distance. A way of measuring similarity between matrices
+    profileNorm = LA.norm(programProfile)  
     programNorm = LA.norm(newProg)
     print("The profile's norm: {}".format(profileNorm))
     print("The current program's norm:{}".format(programNorm))
-    print("Comparison value: {}".format(abs(profileNorm-programNorm)))
+    print("Comparison value: {}".format(abs(profileNorm-programNorm))) #the differences in the norms tells us how close in similarity they are to each other. 0 is the closest similarity.
 
-def interp_matrix(matrix, max_length): #change size of matrix
+def interp_matrix(matrix, max_length): #Add values to matrix to assure its length is the int given by "max_length"
     indices = np.arange(matrix.shape[0])
-    spline = [CubicSpline(indices,matrix[:,i]) for i in range(matrix.shape[1])]
+    spline = [CubicSpline(indices,matrix[:,i]) for i in range(matrix.shape[1])] 
     new_indices = np.linspace(0,indices[-1], max_length)
     interpolated_matrix = np.vstack([interp(new_indices) for interp in spline])
     return(interpolated_matrix)
@@ -39,10 +37,9 @@ def interp_matrix(matrix, max_length): #change size of matrix
 def createProgramMatrices():
     os.chdir(r'folder')
     allMatrices = []
-
-    for file in os.listdir(): #turn each prog file into matrix
+    for file in os.listdir(): #iterate through each file in folder
         studentFile = open(file).read().split("\n")
-        indivMatrix = np.zeros((len(studentFile),len(keys)))
+        indivMatrix = np.zeros((len(studentFile),11)) #n by m matrix, n = numLines; m=num of key programming practices as defined by keys
         StudentSimplified = []
         lineArr = []
         lineNum = 0
@@ -50,28 +47,28 @@ def createProgramMatrices():
             line = line.strip()
             line = line.lower()
             hadKey = False
-            for key in keys:
-                if key in line:
+            for key in keys: 
+                if key in line: #when a line of code is using one of the predefined keywords, change entry at row lineNum, col key from 0 to 1
                         indivMatrix[lineNum][keys[key]] = 1
             lineNum+=1
             StudentSimplified.append(lineArr)
             lineArr = []
         np.set_printoptions(threshold=np.inf)
         allMatrices.append(indivMatrix)
-    return allMatrices
+    return allMatrices #returns a list of each program transformed to a matrix
 
-def toProgramProfile(programMatrices):
+def toProgramProfile(programMatrices): #converts a list of program matrices into a single matrix, or a "program profile"
     max_program_length = max(matrix.shape[0] for matrix in programMatrices) #find longest prog
-    for matrixIndex in range(len(programMatrices)): #interpolate them
+    for matrixIndex in range(len(programMatrices)): #interpolate them to each be the length of the longest program
         programMatrices[matrixIndex] = interp_matrix(programMatrices[matrixIndex],max_program_length)
     numMatrices = len(programMatrices)
-    overall_matrix_weighted = np.divide(programMatrices[0], numMatrices) 
+    overall_matrix_weighted = np.divide(programMatrices[0], numMatrices) #weigh each matrix by the number of files in the folder and sum them
     for matrixIndex in range(1,len(programMatrices)):
         overall_matrix_weighted = np.add(overall_matrix_weighted, np.divide(programMatrices[matrixIndex],numMatrices))
 
-    return(overall_matrix_weighted)
+    return(overall_matrix_weighted) #returns a single numpy matrix, or the "program profile"
     
-def curToMatrix(file,maxLines):
+def curToMatrix(file,maxLines=None): #converts a single file into a program matrix
     testMat = np.zeros((len(file),len(keys)))
     StudentSimplified = []
     lineArr = []
@@ -82,9 +79,11 @@ def curToMatrix(file,maxLines):
         for key in keys:
             if key in line:
                     testMat[lineNum][keys[key]] = 1
-
         lineNum+=1
         StudentSimplified.append(lineArr)
         lineArr = []
-    testMatInterp = interp_matrix(testMat,maxLines)
-    return(testMatInterp)
+    if maxLines == None: 
+        return(testMat)
+    else: #will be used if user wants to add this program to the program profile.
+        testMatInterp = interp_matrix(testMat,maxLines) 
+        return(testMatInterp)
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+
+'''
+Main Program for the program profiler
+replace 'student.txt' with whichever program you choose to comparet to the program profile
+add a folder of past student's work the main directory
+main.py will output the distance between the program profile produced by the folder and the test file
+'''
 import numpy as np
 import os
 from ProgramProfiler import *
 
-np.set_printoptions(threshold=np.inf)
-testf = open(r'student.txt').read().split("\n") #testfile
+testf = open(r'student.txt').read().split("\n") #The file you are choosing to compare replaces "student.txt"
 
-programMatrices = createProgramMatrices()
-progProf = toProgramProfile(programMatrices)
+programMatrices = createProgramMatrices() #converts contents of folder to a list of matrices
+progProf = toProgramProfile(programMatrices) #converts list of matrices to program profile
 
-testMat = curToMatrix(testf,138)
-FrobNorm(progProf,testMat)
+testMat = curToMatrix(testf) #convert single test file into a matrix
+FrobNorm(progProf,testMat) #compare the norms 
 
 
 
